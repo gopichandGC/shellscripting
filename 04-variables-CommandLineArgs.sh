e6ebbdc18c3e906156ee78ecef29a062b45cf169
gopichandGC <gopichandvadlamudi123@gmail.com>
2024-01-30 22:31:07 +0530

    Variables Concept

---
+++ 04-variables-CommandLineArgs.sh
@@ -0,0 +1,6 @@
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: Hello $PERSON2, Good Morning"
+echo "$PERSON2:: Hello $PERSON1, Good Morning"
+echo "$PERSON1:: How are you $PERSON2?"
+echo "$PERSON2:: I am good $PERSON1, How are you"
