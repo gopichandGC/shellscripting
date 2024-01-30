7a3fb9115b4f274eaa2dc1c12808c72c63fbe649
gopichandGC <gopichandvadlamudi123@gmail.com>
2024-01-30 21:44:47 +0530

    Variables Concept

---
+++ 02-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh:: Hello Suresh, Good Morning"
+echo "Suresh:: Hello Ramesh, Good Morning"
+echo "Ramesh:: How are you Suresh?"
+echo "Suresh:: I am good Ramesh, How are you"
