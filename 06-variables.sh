14e795eb4bfd61222806c63cd9b43da727458727
gopichandGC <gopichandvadlamudi123@gmail.com>
2024-02-01 22:59:38 +0530

    Variables

--- 06-variables.sh
+++ 06-variables.sh
@@ -4,5 +4,9 @@
 
 # echo "Username is: $USERNAME, Password is: $PASSWORD"
 echo "Please enter your username:: "
-read USERNAME
-echo "Username is: $USERNAME"
+#read USERNAME
+read -s USERNAME
+echo "Please enter your Password:: "
+#read PASSWORD
+read -s PASSWORD # The value entered above will be automatically attached to username variable
+echo "Username is: $USERNAME, Password is: $PASSWORD"
