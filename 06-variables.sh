fb6ab42f001229a3d2c51edcf85516a3721ec258
gopichandGC <gopichandvadlamudi123@gmail.com>
2024-02-01 22:53:21 +0530

    Variables

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-USERNAME=$1
-PASSWORD=$2
+# USERNAME=$1
+# PASSWORD=$2
 
-echo "Username is: $USERNAME, Password is: $PASSWORD"
+# echo "Username is: $USERNAME, Password is: $PASSWORD"
+echo "Please enter your username:: "
+read USERNAME
+echo "Username is: $USERNAME"
