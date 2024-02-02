9d1e7ac9831897237d4be1b2aece599af7e3d893
gopichandGC <gopichandvadlamudi123@gmail.com>
2024-02-02 15:00:26 +0530

    Optimising the code using Functions

---
+++ 11-functions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+ID=$(id -u)
+
+VALIDATE() {
+
+    if [ $1 -ne 0 ]
+    then
+        echo "ERROR:: $2 ... FAILED"
+        exit 1
+    else
+        echo "$2 ... SUCCESS"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+else
+   echo "You are root user"
+fi
+
+yum install mysql -y
+VALIDATE $? "Installing Mysql"
+
+yum install git -y
+VALIDATE $? "Installing Git"
