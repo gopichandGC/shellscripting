a1b6365743425241878dc3932d9e1ab2dd891b0c
gopichandGC <gopichandvadlamudi123@gmail.com>
2024-02-02 15:20:50 +0530

    Storing the Logs

---
+++ 12-logs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+VALIDATE() {
+
+    if [ $1 -ne 0 ]
+    then
+        echo "ERROR:: $2 ... FAILED"
+        exit 1
+    else
+        echo "$2 ... SUCCESS"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+else
+   echo "You are root user"
+fi
+
+yum install mysql -y &>> $LOGFILE
+VALIDATE $? "Installing Mysql"
+
+yum install git -y &>> $LOGFILE
+VALIDATE $? "Installing Git"
