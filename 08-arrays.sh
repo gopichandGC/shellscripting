ebbf7cbd4c63d822d6492a96beac721f095092a6
gopichandGC <gopichandvadlamudi123@gmail.com>
2024-02-02 14:16:44 +0530

    Arrays Concept

---
+++ 08-arrays.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+FRUITS=("Apple" "Banana" "Mango")
+echo "First Value: ${FRUITS[0]}"
+echo "Second Value: ${FRUITS[1]}"
+echo "Third Value: ${FRUITS[2]}"
+echo "Getting All Values: ${FRUITS[@]}"
