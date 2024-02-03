cff0d05908db61fa2353d8530dc4001ce1fd8a69
gopichandGC <gopichandvadlamudi123@gmail.com>
2024-02-03 12:44:01 +0530

    Installing Packages

---
+++ 15-install-packages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR: Please run this script with root access $N"
+    exit 1
+else
+     echo "You are root user"
+fi
+
+echo "All Arguments passed: $@"
