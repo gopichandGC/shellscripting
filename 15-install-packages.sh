a266129647e96ca43d3a88e2be3a92c57a2cdc99
gopichandGC <gopichandvadlamudi123@gmail.com>
2024-02-03 13:56:06 +0530

    Installing Packages

--- 15-install-packages.sh
+++ 15-install-packages.sh
@@ -5,6 +5,9 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
@@ -24,10 +27,10 @@ fi
 
 for package in $@
 do
-    yum list installed $package
+    yum list installed $package &>> $LOGFILE
     if [ $? -ne 0 ]
     then
-         yum install $package -y
+         yum install $package -y &>> $LOGFILE
          VALIDATE $? "Installation of $package"
     else
          echo -e "$package is already installed ... $Y SKIPPING $N"
