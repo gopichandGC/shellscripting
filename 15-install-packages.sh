0b711b9a671660313bf304523f54daeaf02f9062
gopichandGC <gopichandvadlamudi123@gmail.com>
2024-02-03 12:56:03 +0530

    Installing Packages

--- 15-install-packages.sh
+++ 15-install-packages.sh
@@ -2,8 +2,18 @@
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+         echo -e "$2 ... $R FAILED $N"
+         exit 1
+    else
+         echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
 if [ $ID -ne 0 ]
 then
     echo -e "$R ERROR: Please run this script with root access $N"
@@ -12,5 +22,14 @@ else
      echo "You are root user"
 fi
 
-echo "No.of Arguments passed: $#"
-echo "All Arguments passed: $@"
+for package in $@
+do
+    yum list installed $package
+    if [ $? -ne 0 ]
+    then
+         yum install $package -y
+         VALIDATE $? "Installation of $package"
+    else
+         echo -e "$package is already installed ... $Y SKIPPING $N"
+    fi
+done
